49c77260489c73bb87024c70cb40df6354e89503
Pooja nawale <111230450+PN-74@users.noreply.github.com>
2022-09-04 20:23:17 +0530

    Add files via upload

---
+++ Untitled51.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+def square_num(n):
+    return n * n
+nums = [4, 5, 2, 9]
+print("original list: ", nums)
+result = map(square_num, nums)
+print("square the elements of the said list using map():")
+print(list(result))
+
+
+# In[ ]:
+
+
+
+
